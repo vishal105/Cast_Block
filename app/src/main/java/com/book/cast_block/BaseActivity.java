e93a839f655e7ea6d6cd1bfc0aadc675be2d6613
VISHAL <vishalsojitra.dh@gmail.com>
2018-01-05 08:21:10 +0530

    Initial commit

---
+++ app/src/main/java/com/book/cast_block/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.book.cast_block;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by VISHAL on 1/5/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
