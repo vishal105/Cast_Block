006f1c0ae6d7178aea15c9118a79cfdf28ce2aef
vishal.sojitra <vishal.sojitra@drcsystems.com>
2018-01-05 10:07:12 +0530

    done casting block

--- app/src/main/java/com/book/cast_block/BaseActivity.java
+++ app/src/main/java/com/book/cast_block/BaseActivity.java
@@ -1,26 +1,43 @@
+/*
 package com.book.cast_block;
 
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnDismissListener;
 import android.media.MediaRouter;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
 
+*/
 /**
  * Created by VISHAL on 1/5/2018.
- */
+ *//*
+
 
 public class BaseActivity extends AppCompatActivity {
     private MediaRouter f2554d;
     private DemoPresentation f2557g;
     private final OnDismissListener f2558h = new C03521(this);
 
-    private final MediaRouter.SimpleCallback f2562m = new C03532(this);
+//    private final MediaRouter.SimpleCallback f2562m = new C03532(this);
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        this.f2554d = (MediaRouter) getSystemService("media_router");
+
+//        this.f2554d.addCallback(2, this.f2562m);
+    }
+
 
     class C03521 implements OnDismissListener {
-        final /* synthetic */ BaseActivity f2598a;
+        final */
+/* synthetic *//*
+ BaseActivity f2598a;
 
         C03521(BaseActivity baseActivity) {
             this.f2598a = baseActivity;
@@ -33,30 +50,37 @@ public class BaseActivity extends AppCompatActivity {
         }
     }
 
-    class C03532 extends MediaRouter.SimpleCallback {
-        final /* synthetic */ BaseActivity f2599a;
+   */
+/* class C03532 extends MediaRouter.SimpleCallback {
+        final *//*
+*/
+/* synthetic *//*
+*/
+/* BaseActivity mainActivity;
 
         C03532(BaseActivity baseActivity) {
-            this.f2599a = baseActivity;
+            this.mainActivity = baseActivity;
         }
 
         public void onRouteSelected(MediaRouter mediaRouter, int i, MediaRouter.RouteInfo routeInfo) {
             Log.d("FOO", "onRouteSelected: type=" + i + ", info=" + routeInfo);
-            this.f2599a.mo1529b();
+            this.mainActivity.mo1529b();
         }
 
         public void onRouteUnselected(MediaRouter mediaRouter, int i, MediaRouter.RouteInfo routeInfo) {
             Log.d("FOO", "onRouteUnselected: type=" + i + ", info=" + routeInfo);
-            this.f2599a.mo1529b();
+            this.mainActivity.mo1529b();
         }
 
         public void onRoutePresentationDisplayChanged(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
             Log.d("FOO", "onRoutePresentationDisplayChanged: info=" + routeInfo);
-            this.f2599a.mo1529b();
+            this.mainActivity.mo1529b();
         }
-    }
+    }*//*
 
-    private void mo1529b() {
+
+  */
+/*  private void mo1529b() {
         MediaRouter.RouteInfo selectedRoute = this.f2554d.getSelectedRoute(2);
         Display presentationDisplay = selectedRoute != null ? selectedRoute.getPresentationDisplay() : null;
         if (!(this.f2557g == null || this.f2557g.getDisplay() == presentationDisplay)) {
@@ -73,17 +97,25 @@ public class BaseActivity extends AppCompatActivity {
             }
         }
     }
+*//*
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        this.f2554d.addCallback(2, this.f2562m);
+    }
 
     @Override
     protected void onPause() {
         super.onPause();
-        this.f2554d.removeCallback(this.f2562m);
+//        this.f2554d.removeCallback(this.f2562m);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mo1529b();
-        this.f2554d.addCallback(2, this.f2562m);
+//        mo1529b();
+//        this.f2554d.addCallback(2, this.f2562m);
     }
 }
+*/
--- app/src/main/java/com/book/cast_block/MainActivity.java
+++ app/src/main/java/com/book/cast_block/MainActivity.java
@@ -1,12 +1,93 @@
 package com.book.cast_block;
 
+import android.media.MediaRouter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity {
+    private MediaRouter mediaRouter;
+    private DemoPresentation demoPresentation;
+//    private final DialogInterface.OnDismissListener onDismissListener = new C03521(this);
+
+    private final MediaRouter.SimpleCallback simpleCallback = new C03532(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mediaRouter = (MediaRouter) getSystemService(MEDIA_ROUTER_SERVICE);
+        castMethod();
+    }
+
+  /*  class C03521 implements DialogInterface.OnDismissListener {
+        final *//* synthetic *//* MainActivity f2598a;
+
+        C03521(MainActivity baseActivity) {
+            this.f2598a = baseActivity;
+        }
+
+        public void onDismiss(DialogInterface dialogInterface) {
+            if (dialogInterface == this.f2598a.demoPresentation) {
+                this.f2598a.demoPresentation = null;
+            }
+        }
+    }*/
+
+    class C03532 extends MediaRouter.SimpleCallback {
+        final /* synthetic */ MainActivity mainActivity;
+
+        C03532(MainActivity baseActivity) {
+            this.mainActivity = baseActivity;
+        }
+
+        public void onRouteSelected(MediaRouter mediaRouter, int i, MediaRouter.RouteInfo routeInfo) {
+            Log.d("FOO", "onRouteSelected: type=" + i + ", info=" + routeInfo);
+            this.mainActivity.castMethod();
+        }
+
+        public void onRouteUnselected(MediaRouter mediaRouter, int i, MediaRouter.RouteInfo routeInfo) {
+            Log.d("FOO", "onRouteUnselected: type=" + i + ", info=" + routeInfo);
+            this.mainActivity.castMethod();
+        }
+
+        public void onRoutePresentationDisplayChanged(MediaRouter mediaRouter, MediaRouter.RouteInfo routeInfo) {
+            Log.d("FOO", "onRoutePresentationDisplayChanged: info=" + routeInfo);
+            this.mainActivity.castMethod();
+        }
+    }
+
+    private void castMethod() {
+        MediaRouter.RouteInfo selectedRoute = this.mediaRouter.getSelectedRoute(2);
+        Display presentationDisplay = selectedRoute != null ? selectedRoute.getPresentationDisplay() : null;
+        if (!(this.demoPresentation == null || this.demoPresentation.getDisplay() == presentationDisplay)) {
+            this.demoPresentation.dismiss();
+            this.demoPresentation = null;
+        }
+        if (this.demoPresentation == null && presentationDisplay != null) {
+            this.demoPresentation = new DemoPresentation(this, presentationDisplay);
+//            this.demoPresentation.setOnDismissListener(this.onDismissListener);
+            try {
+                this.demoPresentation.show();
+            } catch (WindowManager.InvalidDisplayException e) {
+                this.demoPresentation = null;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.mediaRouter.addCallback(2, this.simpleCallback);
+        castMethod();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.mediaRouter.removeCallback(this.simpleCallback);
     }
 }
